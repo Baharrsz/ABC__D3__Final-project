aad508f49dd113090514beb04f7f1c00d5282c63
Bahar Raiszadeh <Bahar@Bahars-MBP-2.vc.shawcable.net>
2021-03-16 12:23:56 -0700

    Completed functionality of showing colors of a specified month and option on the map.

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 d3.queue()
-    .defer(d3.csv, "data/covid-data.csv", formatter)
-    .await((error, data) => {
-        let monthsData = getMonths(data);
+    .defer(d3.csv, 'data/covid-data.csv', formatter)
+    .defer(d3.json, 'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json')
+    .await((error, covidData, mapData) => {
+        if (error) console.log(error);
+
+        let months = getMonths(covidData);
+        console.log("month data", months);
+
+        let monthData = months["2021-03"];
+        
+        let clrScaleCase = d3.scaleLinear()
+                            .domain([0,2500])
+                            .range(['white', 'red'])
+
+        let clrScaleDeath = d3.scaleLinear()
+                            .domain([0,30])
+                            .range(['white', 'blue'])                    
+        
+        let geoData = topojson.feature(mapData, mapData.objects.countries).features;
+
+        geoData.forEach(feature => {
+            let found = monthData.find(country => country.location === feature.properties.name);
+            if (found) feature.properties = found;
+        })
+        console.log("geoData", geoData)
+
+        let projection = d3.geoMercator()
+                            .scale(75)
+                            .translate([250,250]);
+        let path = d3.geoPath()
+                        .projection(projection)
+
+        
+
+        let countries = d3.select('svg')
+                            .attr('width', 500)
+                            .attr('height', 500)
+                        .selectAll('.country')
+                            .data(geoData)
+
+        countries
+            .enter()
+                .append('path')
+                .classed('country', true)
+            .merge(countries)
+                .attr('d', path)
+                .attr('fill', d => {
+                    if (d.properties.casesPerMil === undefined) {console.log(d); return 'grey';}
+                    return clrScaleCase(d.properties.casesPerMil)})
+                
+
 
     
 
@@ -28,10 +76,12 @@ function formatter(row, idx, headers){
         isoCode: row.iso_code,
         continent: row.continent,
         date: row.date,
+        cases: +row.new_cases,
+        casesPerMil : +row.new_cases_per_million,
         totalCases: +row.total_cases,
-        newCases: +row.new_cases,
+        deaths: +row.new_deaths,
+        deathsPerMil: +row.new_deaths_per_million,
         totalDeaths: +row.total_deaths,
-        newDeaths: +row.new_deaths,
         population: +row.population,
         medianAge: +row.median_age,
         devIndex: +row.human_development_index
@@ -50,16 +100,20 @@ function getMonths(data) {
                     location: row.location,
                     isoCode: row.isoCode,
                     continent: row.continent,
-                    newCases: row.newCases,
-                    newDeaths: row.newDeaths,
+                    cases: row.cases,
+                    casesPerMil: row.casesPerMil,
+                    deaths: row.deaths,
+                    deathsPerMil: row.deathsPerMil,
                     population: row.population,
                     medianAge: row.medianAge,
                     devIndex: row.devIndex
                 }
                 monthsData[month].push(countryObj)
             } else {
-                foundCountry.newCases += row.newCases;
-                foundCountry.newDeaths += row.newDeaths;
+                foundCountry.cases += row.cases;
+                foundCountry.casesPerMil += row.casesPerMil;
+                foundCountry.deaths += row.deaths;
+                foundCountry.deathsPerMil += row.deathsPerMil;
             }
         })
 
