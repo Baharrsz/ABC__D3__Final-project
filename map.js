4dd557efd165f06f600cb8675e8d9522bbca740c
Bahar Raiszadeh <Bahar@Bahars-MBP-2.vc.shawcable.net>
2021-03-16 23:42:52 -0700

    Moved code for drawing maps to map.js

---
+++ map.js
@@ -0,0 +1,73 @@
+function drawMap(allMonthsData, mapData, monthToShow, dataToShow) {
+    const width = 650;
+    const height = 400;
+    const projectionScale = 100;
+
+    let monthData = allMonthsData[monthToShow];
+
+    let clrScale;
+        if (dataToShow === 'casesPerMil') {
+            clrScale = d3.scaleLinear()
+                            .domain([0,2500])
+                            .range(['lightgrey', '#730e0e']);
+        } else {
+            clrScale = d3.scaleLinear()
+                            .domain([0,100])
+                            .range(['lightgrey', '#0c326b']);
+        }
+        
+                
+    let geoData = topojson.feature(mapData, mapData.objects.countries).features;
+
+    geoData.forEach(feature => {
+        let found = monthData.find(country => country.numericCode === feature.id);
+        if (found) feature.properties = found;
+    })
+
+    let projection = d3.geoMercator()
+                        .scale(projectionScale)
+                        .translate([width / 2, 0.7 * height]);
+    let path = d3.geoPath()
+                    .projection(projection)
+
+    
+
+    let countries = d3.select('.map__chart')
+                        .attr('width', width)
+                        .attr('height', height)
+                    .selectAll('.country')
+                        .data(geoData)
+
+    countries
+        .enter()
+            .append('path')
+            .classed('country', true)
+        .merge(countries)
+            .attr('d', path)
+            .on('mousemove', showMapTooltip)
+            .on('mousout', hideTooltip)
+            .transition()
+                .duration(500)
+                .attr('fill', d => {
+                if (d.properties[dataToShow] === undefined) return 'grey';
+                return clrScale(d.properties[dataToShow])})
+
+    d3.select('.map__title')
+        .html(`
+            <span>${(dataToShow === 'casesPerMil')? 'New Cases of Covid' : 'New Deaths'} per Million Population, </span>
+            <span>${monthToShow}</span>
+        `)
+}
+
+function showMapTooltip(d) {
+    d3.select('.tooltip')
+        .style('opacity', 1)
+        .style('top', `${d3.event.y}px`)
+        .style('left', `${d3.event.x}px`)
+        .html(`
+                <p>Country: ${d.properties.name}</p>
+                <p>Population: ${d.properties.population || 'NA'}</p>
+                <p>New Cases per Million: ${(d.properties.casesPerMil) || 'NA'}</p>
+                <p>New Deaths per Million: ${(d.properties.deathsPerMil) || 'NA'}</p>
+            `);
+}
