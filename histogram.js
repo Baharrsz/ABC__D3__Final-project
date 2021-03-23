872be2b24aa443cb09774db45ab35160cd8e9091
Bahar Raiszadeh <Bahar@Bahars-MBP-2.vc.shawcable.net>
2021-03-23 13:08:31 -0700

    Added axes.

--- histogram.js
+++ histogram.js
@@ -1,7 +1,7 @@
 function drawHistogram(data, countryId, dataType) {
     const width = 500;
     const height = 500;
-    const padding = 50;
+    const padding = 75;
 
 
     
@@ -21,47 +21,79 @@ function drawHistogram(data, countryId, dataType) {
     let monthNames = data.map(obj => obj.month);
 
 
-    let xScale = d3.scaleLinear()
-                    .domain([0, monthNames.length - 1])
-                    .range([padding, width - padding]);
+    let xScale = d3.scalePoint()
+                    .domain(monthNames)
+                    .range([padding, width - padding])
 
     let yScale = d3.scaleLinear()
                     .domain([0, d3.max(data, d => d[dataType])])
                     .range([height - padding, padding])
 
+    drawAxes(xScale, yScale, height, padding, monthNames);
 
 
 
-    let rects = d3.select('.histogram__chart')
+
+    let bars = d3.select('.histogram__chart')
                     .attr('width', width)
                     .attr('height', height)
                 .selectAll('rect')
                     .data(data);
 
 
-    rects
+    bars
         .exit()
         .remove();
 
-    rects
+    bars
         .enter()
         .append('rect')
             .classed('histogram__bin', true)
-        .merge(rects)
-            .attr('x', (d, idx) => xScale(idx))
+        .merge(bars)
+            .attr('x', (d, idx) => xScale(d.month) - 0.5 * (width - 2 * padding) / (monthNames.length - 1))
             .attr('width', (width - 2 * padding) / (monthNames.length - 1))
         .transition()
             .duration(200)
-            .delay((d,idx) => idx * 100)
+            .delay((d,idx) => idx * 50)
             .ease(d3.easeSin)
             .attr('y', height - padding)
             .attr('height', 0)
         .transition()
             .duration(200)
-            .delay((d,idx) => idx * 100)
+            .delay((d,idx) => idx * 50)
             .ease(d3.easeSin)
             .attr('y', d => yScale(d[dataType]))
             .attr('height', d => height - padding - yScale(d[dataType]))
             .attr('fill', (dataType === 'cases')? '#730e0e' : '#0c326b')
+
+}
+
+function drawAxes(xScale, yScale, height, padding, monthNames) {
+
+    d3.selectAll('.axis')
+        .remove();
+
+    let xAxis = d3.axisBottom(xScale)
+                    .tickValues(monthNames);
+    d3.select('.histogram__chart')
+        .append('g')
+            .classed('axis x-axis', true)
+            .attr('transform', `translate(0, ${height - 3 * padding / 4})`)
+            .call(xAxis)
+        .selectAll('text')
+            .attr('transform', 'rotate(90)')
+            .attr('text-anchor', 'start')
+            .attr("y", 0)
+            .attr("x", 9)
+            .attr("dy", ".35em")
+
+    let yAxis = d3.axisLeft(yScale);
+    d3.select('.histogram__chart')
+        .append('g')
+            .classed('axis y-axis', true)
+            .attr('transform', `translate(${3* padding / 4}, 0)`)
+            .call(yAxis)
+
+
 
 }
